e9feac2d5d2f6aecdd8b522331cf73f9925f93dd
Ari Horowitz <ariehh@comcast.net>
2020-04-24 21:09:40 -0700

    reconfigured booksController.js with no date

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,23 +2,19 @@ import axios from "axios";
 
 const BASEURL = "https://www.googleapis.com/books/v1/volumes?q=";
 
-// Export an object with a "search" method that searches the Giphy API for the passed query
 export default {
   search: function (query) {
     return axios.get(BASEURL + query);
   },
 
-  // Saves a book to the database
   saveBook: function (bookData) {
     return axios.post("/api/search", bookData);
   },
 
-  // Gets all books that matches the search
   getBooks: function (query) {
     return axios.get(`/api/search/${query || ""}`);
   },
 
-  // Deletes the book with the given id
   deleteBook: function (id) {
     return axios.delete("/api/search/" + id);
   },
