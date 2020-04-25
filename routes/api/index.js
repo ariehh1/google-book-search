43236ce8a36c2f66310b923d72d0ad70d7378d27
Ari Horowitz <ariehh@comcast.net>
2020-04-24 21:16:20 -0700

    renamed file within api folder

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,5 +1,5 @@
 const router = require(`express`).Router();
-const bookRoutes = require(`./books`);
+const bookRoutes = require(`./search`);
 
 // Book routes
 router.use(`/books`, bookRoutes);
--- routes/api/search.js
+++ routes/api/search.js
@@ -7,7 +7,7 @@ router.route(`/`).get(booksController.findAll).post(booksController.create);
 // Matches with "/api/books/:id"
 router
   .route(`/:id`)
-  .get(booksController.findById)
+  .get(booksController.findMany)
   .put(booksController.update)
   .delete(booksController.remove);
 
