7eed05cd38191c0d253894dbe684784c68ac31fa
Ari Horowitz <ariehh@comcast.net>
2020-04-26 11:24:52 -0700

    testing heroku deployment in server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === `production`) {
 // Add routes, both API and view
 app.use(routes);
 
-// Connect to the Mongo DB
+// Connect to the Mongo DB here
 mongoose.connect(
   process.env.MONGODB_URI || `mongodb://localhost/reactreadinglist`,
   {
