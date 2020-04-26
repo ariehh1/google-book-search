047817df60c99a94982087749f54103a4deecf8e
Ari Horowitz <ariehh@comcast.net>
2020-04-26 10:09:20 -0700

    saved books render on page, view in google api, and delete from page

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,6 +2,6 @@ const router = require("express").Router();
 const bookRoutes = require("./search");
 
 // Book routes
-router.use("/books", bookRoutes);
+router.use("/search", bookRoutes);
 
 module.exports = router;
